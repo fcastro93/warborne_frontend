0ae43ec0920af78764406f20ebfba6dd0969dac3
Fernando Castro Mendez <fcastro.mendez.work@gmail.com>
2025-09-22 20:05:31 -0600

    Remove AppNavbar component completely - delete top toolbar with menu and notifications

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import SideMenu from './SideMenu';
-import AppNavbar from './AppNavbar';
 
 export default function Layout({ children }) {
   return (
     <Box sx={{ display: 'flex' }}>
       <SideMenu />
-      <AppNavbar />
       <Box
         component="main"
         sx={{
